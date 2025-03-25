7a2eeb5a87baff13245c75b73cd90b47ea89e6ed
BICHO128 <davidurrutiaceron200507@gmail.com>
2025-03-24 18:22:37 -1200

    Version 1.0

---
+++ src/components/TodoApp.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { ListaTareas } from './ListaTareas';
+export const TodoApp = () => {
+
+    const [nuevaTarea, setNuevaTarea] = useState<string>('')
+    const [listaTareas, setListaTareas] = useState<string[]>([])
+
+
+
+    const handleAddTask = () => {
+        if (nuevaTarea.trim() === '') return
+        setListaTareas(tareasAnteriores => [...tareasAnteriores, nuevaTarea])
+        setNuevaTarea('')
+    }
+
+    const handleBorrarTarea = (index: number) => {
+        setListaTareas(tareas => tareas.filter((_, i) => i !== index))
+    }
+
+    return (
+        <div>
+            <h1>Lista de Tareas</h1>
+            <h2>Utilizando React con TypeScript</h2>
+            <h3>Tareas Pendientes</h3>
+            <p>{listaTareas.length}</p>
+
+            <div className="flex">
+                <input
+                    type="text"
+                    value={nuevaTarea}
+                    onChange={(e) => setNuevaTarea(e.target.value)}
+                    placeholder="Nueva Tarea"
+                />
+                <button onClick={handleAddTask}>Agregar Tarea</button>
+            </div>
+            <ListaTareas
+                listaTareas={listaTareas}
+                borrarTarea={handleBorrarTarea}
+            ></ListaTareas>
+        </div>
+    );
+}
